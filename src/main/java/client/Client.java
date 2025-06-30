532589dcfd00e56a79745dc33ffbfbcb8079a4f4
Erneo97 <michalkaniewski1997@gmail.com>
2025-06-30 17:25:54 +0200

    Przesłanie  komunikatu do serwera z listą plików po stronie klienta

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,16 +1,25 @@
 package client;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Scanner;
 
+import announcements.FileInformation;
+import announcements.InitClientToServer;
+import universal.CommunicateManager;
+import universal.ConverterFilesIngormationToJson;
+import universal.FileManager;
+
 public class Client {
+    Socket socket = null;
+    CommunicateManager communicateManager;
 
     public boolean loginToServer() {
         Scanner scanner = new Scanner(System.in);
-
-        Socket socket = null;
 
         while (socket == null || !socket.isConnected()) {
             System.out.print("Podaj adres IP serwera: ");
@@ -20,25 +29,62 @@ public class Client {
             int port = scanner.nextInt();
             scanner.nextLine();
 
+            // TODO: usunąć te dwie linie po zakonczeniu testów
+            serverIP = "localhost";
+            port = 1234;
+
+
             try {
                 socket = new Socket();
                 socket.connect(new InetSocketAddress(serverIP, port), 5000);
-                System.out.println("Połączono z serwerem.");
+                communicateManager = new CommunicateManager(socket);
             } catch (IOException e) {
                 System.out.println("Nie udało się połączyć w ciągu 5 sekund. Spróbuj ponownie.\n");
+                return false;
             }
         }
-
+        System.out.println("Połączono z serwerem.");
         return true;
     }
+
 
     public static void main(String[] args) {
         Client client = new Client();
 
         client.loginToServer();
 
+
+        FileManager fileManager = new FileManager("client_data");
+        System.out.println("Odczytuje twój zbiór lokalny...");
+        List<FileInformation> informationFiles = fileManager.getListOfFilesInformation();
+
+        System.out.println("Twoja lista plików:\n");
+        Client.printList(informationFiles);
+
+        InitClientToServer firstCommunicate = new InitClientToServer();
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            firstCommunicate.IP  = localHost.getHostAddress();
+            firstCommunicate.filesInformation = informationFiles;
+
+            String jsonToSEND = ConverterFilesIngormationToJson.convertInitClientToServer(firstCommunicate);
+            client.communicateManager.sendCommunicate(jsonToSEND);
+
+
+        } catch (UnknownHostException e) {
+            System.out.println("Nie można uzyskać adresu IP: " + e.getMessage());
+        }
+
+
     }
 
+    private static void printList(List<FileInformation> list) {
+        int index = 0;
+        for (FileInformation fileInformation : list) {
+            System.out.println(index + ") " + fileInformation);
+            index++;
+        }
+    }
 
 
 }
--- src/main/java/server/UserHandling.java
+++ src/main/java/server/UserHandling.java
@@ -2,21 +2,31 @@ package server;
 
 import java.io.IOException;
 import java.net.Socket;
+
+import announcements.InitClientToServer;
 import database.Manager_db;
+import universal.CommunicateManager;
+import universal.ConverterFilesIngormationToJson;
 
 
 public class UserHandling implements Runnable {
     private Socket socket;
     private Manager_db managerDatabse;
+    private CommunicateManager communicateManager;
 
     UserHandling(Socket socket) {
         this.socket = socket;
+        this.communicateManager = new CommunicateManager(socket);
     }
 
     @Override
     public void run() {
         System.out.println("UserHandling started o adrersie" + socket.getInetAddress());
 
+        String initJsonFromClient =  communicateManager.receiveCommunicate();
+        InitClientToServer comunicate = ConverterFilesIngormationToJson.restoreInitClientToServer(initJsonFromClient);
+
+        System.out.println("Pierwszy komunikat:\n" + comunicate);
 
         try {
             socket.close();
