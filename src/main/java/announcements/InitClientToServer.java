5517b2182584d72e4ea92c33477baf9d3e612b46
Erneo97 <michalkaniewski1997@gmail.com>
2025-06-30 16:37:44 +0200

    Dodanie wyświetlania komunikatu initcjującego komunikatu klienta: IP + wszystkie informacje

--- src/main/java/announcements/InitClientToServer.java
+++ src/main/java/announcements/InitClientToServer.java
@@ -6,4 +6,15 @@ public class InitClientToServer {
     public String IP;
 
     public List<FileInformation> filesInformation;
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder("\t" + this.IP + " :\n");
+        int index = 0;
+        for (FileInformation fileInformation : this.filesInformation) {
+            ret.append(index).append(") ").append(fileInformation.toString()).append("\n");
+            index++;
+        }
+        return ret.toString();
+    }
 }
