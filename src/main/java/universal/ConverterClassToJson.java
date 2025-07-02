edcb7ebfd80bb081f25df0a309ffea071e116c8b
Erneo97 <michalkaniewski1997@gmail.com>
2025-07-02 11:21:01 +0200

    Dokumentacja dla klas pomocniczych Formatowanie tekstu Konwersji typów na json i z json Porównywanie stanów dwóch list informacji o plikach

--- src/main/java/universal/ConverterClassToJson.java
+++ src/main/java/universal/ConverterClassToJson.java
@@ -6,32 +6,71 @@ import com.google.gson.Gson;
 import universal.models.NextSyncTime;
 import universal.models.StateServer;
 
-
+/**
+ * Klasa pomocnicza do konwersji obiektów Java na JSON oraz odwrotnie.
+ * Wykorzystuje bibliotekę Gson do serializacji i deserializacji.
+ */
 public class ConverterClassToJson {
 
+    /**
+     * Konwertuje dowolny obiekt na reprezentację JSON w postaci Stringa.
+     *
+     * @param filesInformation obiekt do konwersji na JSON
+     * @return łańcuch znaków w formacie JSON reprezentujący obiekt
+     */
     public static String convert(Object filesInformation) {
         Gson gson = new Gson();
         String json = gson.toJson(filesInformation);
-
         return json;
     }
 
+    /**
+     * Odtwarza obiekt typu {@link ListClientsFiles} z podanego JSON-a.
+     *
+     * @param communicate JSON do deserializacji
+     * @return odtworzony obiekt ListClientsFiles
+     */
     public static ListClientsFiles restoreFileInformation(String communicate) {
-        return (ListClientsFiles)restoreDefault(communicate, ListClientsFiles.class);
+        return (ListClientsFiles) restoreDefault(communicate, ListClientsFiles.class);
     }
 
+    /**
+     * Odtwarza obiekt typu {@link InitClientToServer} z podanego JSON-a.
+     *
+     * @param communicate JSON do deserializacji
+     * @return odtworzony obiekt InitClientToServer
+     */
     public static InitClientToServer restoreInitClientToServer(String communicate) {
-        return (InitClientToServer)restoreDefault(communicate, InitClientToServer.class);
+        return (InitClientToServer) restoreDefault(communicate, InitClientToServer.class);
     }
 
+    /**
+     * Odtwarza wartość enum {@link StateServer} z podanego JSON-a.
+     *
+     * @param communicate JSON do deserializacji
+     * @return odtworzony enum StateServer
+     */
     public static StateServer restoreStateServer(String communicate) {
-        return (StateServer)restoreDefault(communicate, StateServer.class);
+        return (StateServer) restoreDefault(communicate, StateServer.class);
     }
 
+    /**
+     * Odtwarza wartość enum {@link NextSyncTime} z podanego JSON-a.
+     *
+     * @param communicate JSON do deserializacji
+     * @return odtworzony enum NextSyncTime
+     */
     public static NextSyncTime restoreNextSyncTime(String communicate) {
-        return (NextSyncTime)restoreDefault(communicate, NextSyncTime.class);
+        return (NextSyncTime) restoreDefault(communicate, NextSyncTime.class);
     }
 
+    /**
+     * Metoda pomocnicza deserializująca JSON do obiektu podanego typu.
+     *
+     * @param communicate JSON do deserializacji
+     * @param klasa klasa docelowego typu
+     * @return odtworzony obiekt typu podanego jako parametr
+     */
     private static Object restoreDefault(String communicate, Class<?> klasa) {
         Gson gson = new Gson();
         return gson.fromJson(communicate, klasa);
--- src/main/java/universal/FormaterTerminalText.java
+++ src/main/java/universal/FormaterTerminalText.java
@@ -1,32 +1,89 @@
 package universal;
 
+/**
+ * Klasa pomocnicza do formatowanego wyświetlania tekstu w terminalu.
+ * Umożliwia wypisywanie tekstu z kolorami i wyróżnieniami takimi jak pogrubienie.
+ * Wykorzystuje kody ANSI do formatowania tekstu.
+ */
 public class FormaterTerminalText {
+    /**
+     * Resetowanie formatu tekstu (przywrócenie domyślnego stylu).
+     */
     public static final String ANSI_RESET = "\u001B[0m";
+
+    /**
+     * Kolor czerwony (używany np. do błędów).
+     */
     public static final String ANSI_RED = "\u001B[31m";
+
+    /**
+     * Kolor zielony (np. do komunikatów o powodzeniu).
+     */
     public static final String ANSI_GREEN = "\u001B[32m";
+
+    /**
+     * Kolor żółty (np. do informacji i komunikatów serwera).
+     */
     public static final String ANSI_YELLOW = "\u001B[33m";
 
+    /**
+     * Pogrubienie tekstu.
+     */
     public static final String ANSI_BOLD = "\u001B[1m";
+
+    /**
+     * Cofnięcie pogrubienia tekstu.
+     */
     public static final String ANSI_UNBOLD = "\u001B[21m";
+
+    /**
+     * Kolor cyjan (niebiesko-zielony, do wyróżnień).
+     */
     public static final String ANSI_CYAN = "\u001B[36m";
 
-    public static void printServerComunicate(String text ) {
+    /**
+     * Wypisuje komunikat serwera w kolorze żółtym i pogrubioną czcionką, poprzedzony tabulatorem.
+     *
+     * @param text tekst do wyświetlenia
+     */
+    public static void printServerComunicate(String text) {
         System.out.println("\t" + ANSI_YELLOW + ANSI_BOLD + text + ANSI_UNBOLD + ANSI_RESET);
     }
 
-    public static void printSucess(String text ) {
+    /**
+     * Wypisuje komunikat sukcesu w zielonym i pogrubionym stylu.
+     *
+     * @param text tekst do wyświetlenia
+     */
+    public static void printSucess(String text) {
         System.out.println(ANSI_GREEN + ANSI_BOLD + text + ANSI_UNBOLD + ANSI_RESET);
     }
 
-    public static void printFailure(String text ) {
+    /**
+     * Wypisuje komunikat błędu w czerwonym i pogrubionym stylu.
+     *
+     * @param text tekst do wyświetlenia
+     */
+    public static void printFailure(String text) {
         System.out.println(ANSI_RED + ANSI_BOLD + text + ANSI_UNBOLD + ANSI_RESET);
     }
 
-    public static void printNormal(String text ) {
+    /**
+     * Wypisuje zwykły, nieformatowany tekst.
+     *
+     * @param text tekst do wyświetlenia
+     */
+    public static void printNormal(String text) {
         System.out.println(text);
     }
 
-    public static void printTextInputs(String text ) {
+    /**
+     * Wypisuje tekst zachęty do wpisania danych (np. zapytanie), w kolorze cyjan i pogrubione.
+     * Nie dodaje nowej linii na końcu, aby umożliwić wpis użytkownika w tej samej linii.
+     *
+     * @param text tekst zachęty
+     */
+    public static void printTextInputs(String text) {
         System.out.print(ANSI_CYAN + ANSI_BOLD + text + ANSI_UNBOLD + ANSI_RESET);
     }
 }
